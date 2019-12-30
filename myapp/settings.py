7745f7ce9076cfe9eb66af24262f65b063db5c67
amir <amir.ranabhat@gmail.com>
2019-12-30 10:03:36 +0545

    index.html changed and caousel updated

--- myapp/settings.py
+++ myapp/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -114,7 +114,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
- 
