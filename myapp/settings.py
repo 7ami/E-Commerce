6ff851fc54f551d613ac7b7aaf48ff562056cae9
amir <amir.ranabhat@gmail.com>
2020-01-05 22:08:47 +0545

    changed db to postgres

--- myapp/settings.py
+++ myapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'greennettdb',
+        'USER': 'postgres',
+        'PASSWORD': 'newpassword1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
